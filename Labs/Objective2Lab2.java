2c65eff70ce3a67af97888af937c91cb3eeaa5c0
DustinMC1802 <dustinmcollier@gmail.com>
2022-02-14 07:27:24 -0800

    complete objective2lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,15 @@
+public class Objective2Lab2 {
+  public static void main(String [] args) {
+    String name = "Dustin Collier";
+    int age = 35;
+    String state = "Kentucky";
+
+    System.out.println("Name: ");
+    System.out.println(name);
+    System.out.println("Age: ");
+    System.out.println(age);
+    System.out.println("State: ");
+    System.out.println(state);
+
+  }
+}
